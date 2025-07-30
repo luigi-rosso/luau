ea81305d96bbef6ae79cf118e8e1a78445ee6af9
Luigi Rosso <luigi.rosso@gmail.com>
2025-07-30 11:56:30 -0700

    chore: make Frontend::check public

--- Analysis/include/Luau/Frontend.h
+++ Analysis/include/Luau/Frontend.h
@@ -234,7 +234,6 @@ struct Frontend
     std::optional<CheckResult> getCheckResult(const ModuleName& name, bool accumulateNested, bool forAutocomplete = false);
     std::vector<ModuleName> getRequiredScripts(const ModuleName& name);
 
-private:
     ModulePtr check(
         const SourceModule& sourceModule,
         Mode mode,
@@ -244,7 +243,8 @@ private:
         bool recordJsonLog,
         TypeCheckLimits typeCheckLimits
     );
-
+    
+private:
     std::pair<SourceNode*, SourceModule*> getSourceNode(const ModuleName& name);
     SourceModule parse(const ModuleName& name, std::string_view src, const ParseOptions& parseOptions);
 
