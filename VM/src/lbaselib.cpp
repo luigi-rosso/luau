7847c465d4fdec04783cdb14436e933df5e47f90
Luigi Rosso <luigi.rosso@gmail.com>
2025-08-07 14:38:16 -0700

    feature: adding RIVE_LUAU definition

--- VM/src/lbaselib.cpp
+++ VM/src/lbaselib.cpp
@@ -13,6 +13,7 @@
 
 LUAU_FASTFLAG(LuauStacklessPcall)
 
+#ifndef RIVE_LUAU
 static void writestring(const char* s, size_t l)
 {
     fwrite(s, 1, l, stdout);
@@ -33,7 +34,7 @@ static int luaB_print(lua_State* L)
     writestring("\n", 1);
     return 0;
 }
-
+#endif
 static int luaB_tonumber(lua_State* L)
 {
     int base = luaL_optinteger(L, 2, 10);
@@ -442,7 +443,7 @@ static int luaB_tostring(lua_State* L)
     luaL_tolstring(L, 1, NULL);
     return 1;
 }
-
+#ifndef RIVE_LUAU
 static int luaB_newproxy(lua_State* L)
 {
     int t = lua_type(L, 1);
@@ -460,7 +461,7 @@ static int luaB_newproxy(lua_State* L)
 
     return 1;
 }
-
+#endif
 static const luaL_Reg base_funcs[] = {
     {"assert", luaB_assert},
     {"error", luaB_error},
@@ -468,8 +469,10 @@ static const luaL_Reg base_funcs[] = {
     {"getfenv", luaB_getfenv},
     {"getmetatable", luaB_getmetatable},
     {"next", luaB_next},
+#ifndef RIVE_LUAU
     {"newproxy", luaB_newproxy},
     {"print", luaB_print},
+#endif
     {"rawequal", luaB_rawequal},
     {"rawget", luaB_rawget},
     {"rawset", luaB_rawset},
